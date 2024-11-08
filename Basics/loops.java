7e1ff31525c45113de22b7f54a6f3943d39cff49
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2024-11-08 23:47:36 +0530

    Add files via upload

---
+++ Basics/loops.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] args) {
+        
+        // for loop
+        // for(int i=0;i<11;i++){
+        //     System.out.print(i+" ");
+        // }
+
+
+
+        // While
+        // int num=10;
+        // while(num>=4){
+        //     System.out.print(num+" ");
+        //     num--;
+        // }
+
+
+        int num=10;
+        do{
+            System.out.print(num);
+            num--;
+        }
+
+        while(num>=4);
+    }
+}
---
+++ Basics/variables.java
@@ -0,0 +1,13 @@
+class variables {
+    public static void main(String args[]){
+
+        int num1=10;
+        int num2=30;
+        System.out.println(num1+num2);
+
+
+        String fname="Vishal";
+        String lname="Panwar";
+        System.out.println(fname+" "+lname); 
+    }
+}
