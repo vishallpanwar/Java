73878a93b73a1d2a744fac67395d3ffd7598695e
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2025-03-13 23:15:31 +0530

    Add files via upload

---
+++ Basics/conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int age=20;
+        if(age<8){
+            System.out.println("Not eligible");
+        }
+
+        else{
+            System.out.println("Dede bhai");
+        }
+    }
+}
