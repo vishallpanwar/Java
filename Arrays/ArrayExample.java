4ae7dfc0ac1460e853ad1c3debacca020d27fdac
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2025-04-07 20:30:56 +0530

    Delete Arrays directory

--- Arrays/ArrayExample.java
+++
@@ -1,9 +0,0 @@
-package Arrays;
-
-public class ArrayExample {
-    public static void main(String[] args) {
-        int nums[] = {1, 2, 3, 4};
-        nums[0] = 66;
-        System.out.println(nums[1]);
-    }
-}
